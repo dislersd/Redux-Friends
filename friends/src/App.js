4261385ca2ade9bc6964b227162f0942573cabe4
dislersd <dislersd@gmail.com>
2019-03-07 14:56:35 -0800

    set up private route to friends list

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from './components/Login'
+import FriendsList from './components/FriendsList'
+import PrivateRoute from './components/PrivateRoute'
 //
 import "./css/App.css";
 
@@ -9,8 +11,10 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-        <Link to='/login'> Login </Link>
+        <Link to='/login'> <button>Login</button> </Link>
+        <Link to='/friends-list'> <button>FriendsList</button> </Link>
         <Route path='/login' component={Login} />
+        <PrivateRoute path='/friends-list' component={FriendsList} />
         </div>
       </Router>
     );
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FriendsList = props => {
+ return (
+   <p> Hello! </p>
+ )
+}
+
+export default FriendsList;
